4452d6b45f4ea58baa09a61c8b1d58343a85cea6
Boyoung Park <qhdudlove321@naver.com>
2016-10-06 01:00:16 -0700

    last file

--- hello.c
+++ hello.c
@@ -1,11 +1,12 @@
 #include<stdio>
 void foo()
 {
+	printf("my name is boyoung");
 	printf("it is foo function");
 }
 void main()
 {
-	printf("it is new debug123");
+	printf("it is new debug");
 	printf("bye bye master pointer!");
 	printf("this is my computer!");
 }
