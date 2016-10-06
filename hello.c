da801fdee6d0feb984d0dde2298823af3cc1880a
Boyoung Park <qhdudlove321@naver.com>
2016-10-06 00:58:04 -0700

    it is debug345

--- hello.c
+++ hello.c
@@ -2,10 +2,12 @@
 void foo()
 {
 	printf("I'm sleepy");
+	printf("you can't sleep anymore");
 }
 void main()
 {
 	printf("it is new debug345");
 	printf("I want to sleep more");
+	printf("OK!!");
 	printf("this is my computer!");
 }
